a7ba2033ccc9cf54844dfbd739a660c5900b1f31
FortizFil <vfilchenko95@gmail.com>
2022-05-22 12:22:58 +0300

    Minor fix server

--- bin/server.js
+++ bin/server.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
-
 const app = require("../app");
-
-const { DB_HOST, PORT = 4000 } = process.env;
+const { DB_HOST, PORT } = require("../config");
 
 mongoose
   .connect(DB_HOST)
---
+++ config/index.js
@@ -0,0 +1,8 @@
+DB_HOST =
+  "mongodb+srv://Fortiz:qweqweqwe@cluster0.tufey.mongodb.net/todos?retryWrites=true&w=majority";
+PORT = 4000;
+
+module.exports = {
+  DB_HOST,
+  PORT,
+};
